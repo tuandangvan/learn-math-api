528e310a1f08c04821fdac9e4e0509f4858060ac
VanTuan <tuandangvann@gmail.com>
2024-05-03 17:12:56 +0700

    off upload

--- src/routes/v1/documentRoute.js
+++ src/routes/v1/documentRoute.js
@@ -1,14 +1,14 @@
-// import express from 'express';
-// import authencation from '../../middlewares/authencationHandingMiddleware';
-// import permission from '../../middlewares/authorizationHandlingMiddelware';
-// import Role from '../../utils/enums';
-// import multer from 'multer';
-// import { documentController } from '../../controllers/documentController';
+import express from 'express';
+import authencation from '../../middlewares/authencationHandingMiddleware';
+import permission from '../../middlewares/authorizationHandlingMiddelware';
+import Role from '../../utils/enums';
+import multer from 'multer';
+import { documentController } from '../../controllers/documentController';
 
-// const upload = multer({ dest: 'uploads' });
-// const router = express.Router();
+const upload = multer({ dest: 'uploads' });
+const router = express.Router();
 // //general
 // router.post("/document", authencation, permission([Role.ADMIN, Role.TEACHER]), upload.single('file'), documentController.createDocument);
 // router.get("/document/:documentId", documentController.getDocument);
 
-// export const documentRouter = router;
+export const documentRouter = router;
